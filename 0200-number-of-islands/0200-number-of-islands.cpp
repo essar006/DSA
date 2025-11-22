20a41943e7780620d7a05c926c3115c38e4d0726
Sabyasachi Rath <47927889+essar006@users.noreply.github.com>
2025-11-23 03:18:07 +0530

    Time: 22 ms (88.2%), Space: 16.2 MB (89.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    vector<vector<bool>> vis;
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.size() || col >= grid[0].size() || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0'; 
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         if (grid.empty()) return 0;
-        int m = grid.size();
-        int n = grid[0].size();
-
         int count = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        vis = vector<vector<bool>>(m, vector<bool>(n, false));
-
-        queue<pair<int, int>> q;
-        vector<pair<int, int>> dirs = {{0, 1},{0, -1},{1, 0},{-1, 0}};
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    q.push({i, j});
-                    while(!q.empty()){
-                        auto var = q.front();
-                        int x = var.first;
-                        int y = var.second;
-                        q.pop();
-                        for(auto& dir : dirs){
-                            int row = x + dir.first;
-                            int col = y + dir.second;
-                            if(row >= 0 && row < m && col >= 0 && col < n && vis[row][col] == false && grid[row][col] == '1'){
-                                vis[row][col] = true;
-                                q.push({row, col});
-                            }
-                        }
-                    }
-                    count++;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1') {
+                    ++count;
+                    dfs(grid, i, j);
                 }
             }
         }
