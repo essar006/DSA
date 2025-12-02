b1e36eb82f065ea341111297910c4a2e4c487bdb
Sabyasachi Rath <47927889+essar006@users.noreply.github.com>
2025-12-02 16:01:52 +0530

    Time: 0 ms (100%), Space: 10.9 MB (29.18%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-vector<vector<int>> res;
-    void dfs(vector<int>& nums, int idx){
-        if(idx == nums.size()){
-            res.push_back(nums);
-            return;
-        }
-        for(int i=idx; i<nums.size(); i++){
-            swap(nums[idx], nums[i]);
-            dfs(nums, idx+1);
-            swap(nums[idx], nums[i]);
+    vector<vector<int>> res;
+    void dfs(vector<int>& nums, int n, int idx){
+        if(idx == n) res.push_back(nums);
+        for(int i=idx; i<n; i++){
+            swap(nums[i], nums[idx]);
+            dfs(nums, n, idx+1);
+            swap(nums[i], nums[idx]);
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        dfs(nums, 0);
+        dfs(nums, nums.size(), 0);
         return res;
     }
 };
