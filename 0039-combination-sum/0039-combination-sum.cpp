c80db47c3d5c38ba59f1c185b9d30b3a99847095
Sabyasachi Rath <47927889+essar006@users.noreply.github.com>
2025-12-02 15:23:33 +0530

    Time: 4 ms (39.58%), Space: 10.9 MB (100%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void dfs(vector<int>& arr, int tar, vector<int>& v, int idx){
-
-        if(tar == 0) {res.push_back(v); return;}
-        if(idx == arr.size() || tar < 0) return;
-
-        dfs(arr, tar, v, idx+1);
-
-        v.push_back(arr[idx]);
-        dfs(arr, tar-arr[idx], v, idx);
+    vector<vector<int>> ans;
+    vector<int> v;
+    void solve(vector<int>& a,int sum,int ind,int n)
+    {
+        if(sum==0)
+        {
+            ans.push_back(v);
+            return;
+        }
+        
+        if(ind==n || sum<0)
+        {
+            return;
+        }
+       
+        v.push_back(a[ind]);
+        solve(a,sum-a[ind],ind,n);
         v.pop_back();
+        
+        solve(a,sum,ind+1,n);
     }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> v;
-        dfs(candidates, target, v, 0); 
-
-        return res;
+    
+    vector<vector<int>> combinationSum(vector<int>& a, int k) {
+        
+        int n=a.size();
+        solve(a,k,0,n);   
+        return ans;
     }
 };
